36a0f9e9d15db18143d3ffa1fa25b3368b25eb66
Akmal Bintang <akmalbintang33@gmail.com>
2025-05-15 14:53:21 +0700

    fix button setting

--- src/pages/ecohive/ProfilePage.jsx
+++ src/pages/ecohive/ProfilePage.jsx
@@ -127,7 +127,7 @@ const ProfilePage = () => {
       animate="animate"
       exit="exit"
       variants={pageVariants}
-      className="max-w-4xl mx-auto py-6 px-4 sm:px-6 lg:px-8"
+      className="max-w-4xl mx-auto py-6 px-4 sm:px-6 lg:px-2"
     >
       <motion.div variants={itemVariants} className="mb-6">
         <h1 className="text-2xl font-bold text-teal-900 dark:text-lime-500">
